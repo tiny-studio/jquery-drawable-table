ddb9305024f31e3c1a7d2393d379dd637defa387
tiny-studio <tanaka@example.com>
2014-01-26 15:57:57 +0900

    スマートフォンでもドラッグできるよう修正した。

--- src/jquery.drawable-table.js
+++ src/jquery.drawable-table.js
@@ -1,25 +1,25 @@
 /*
  * The MIT License (MIT)
-
-Copyright (c) 2014 Tomoya Tanaka
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+ 
+ Copyright (c) 2014 Tomoya Tanaka
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
  */
 (function($) {
     $.fn.drawableTable = function(options) {
@@ -39,16 +39,31 @@ THE SOFTWARE.
                     settings.draw($(this));
                 }
             };
-            $table.mousedown(start).mouseup(end).mouseleave(end);
-            $table.find("td").mouseover(draw).click(function(){
-                settings.draw($(this));
-            });
+            var offset = $table.offset();
+            var xUnit = $table.width() / $table.find("tr:first-child td").size();
+            var yUnit = $table.height() / $table.find("tr").size();
+            $table
+                    .on('mousedown', start)
+                    .on('mouseup mouseleave', end)
+                    .on('touchmove', function(event) {
+                        settings.ontouchmove.call(this, event, settings.draw, offset, xUnit, yUnit);
+                    })
+                    .find("td").mouseover(draw).on("click touchstart", function(){settings.draw($(this));});
         });
     };
 
     $.fn.drawableTable.defaults = {
         draw: function($td) {
             $td.addClass("drawn");
+        },
+        ontouchmove: function(event, draw, offset, xUnit, yUnit) {
+            var $table = $(this);
+            var touch = event.originalEvent.touches[0];
+            var x = Math.floor((touch.clientX - offset.left) / xUnit) + 1;
+            var y = Math.floor((touch.clientY - offset.top) / yUnit) + 1;
+            var $td = $table.find("tr:nth-child(" + y + ")").find("td:nth-child(" + x + ")");
+            draw($td);
+            event.preventDefault();
         }
     };
 })(jQuery);
